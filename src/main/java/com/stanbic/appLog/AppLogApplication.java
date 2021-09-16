f65f014823bdbea8b5bd0a116f8474c3435d220e
Benson Kariuki <kariuki.benson@azenia.com>
2021-09-16 09:30:32 +0300

    initial commit

---
+++ src/main/java/com/stanbic/appLog/AppLogApplication.java
@@ -0,0 +1,13 @@
+package com.stanbic.appLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stanbic/appLog/AppLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stanbic.appLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
